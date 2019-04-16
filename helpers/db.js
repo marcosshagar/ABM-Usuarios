a00d1932b3a042dd4259988170f72cee9400b965
marcos shanahan <marcosshagar@gmail.com>
2019-04-16 20:36:49 -0300

    prueba

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const config = require('config.json');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true }, function() {
+mongoose.connect(process.env.MONGOLAB_PURPLE_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true }, function() {
     console.log("El Servidor MongoDB esta Conectado");
 });
 
