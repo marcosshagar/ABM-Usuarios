1f6f0ac6a6894b2135d2bf38c256086f7801641e
marcosshagar <marcosshagar@gmail.com>
2019-04-30 13:48:18 -0300

    Se integro la API de MercadoPago

--- payments/paymentController.js
+++ payments/paymentController.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var paymentService = require('./paymentService')
+var request = require('superagent');
 
-router.get('/pay', payments);
+router.post('/pay', payments);
 
 module.exports = router;
 
 function payments(req, res, next) {
     console.log("Ingreso al controlador para pagar");
-    //console.log(req.body);
-    paymentService(req.body)
-        .then(function () {
-                res.json({ message: "Se realizo el pago correctamente."});
+
+    console.log("Comienzo el request");
+
+    request.post("https://api.mercadopago.com/v1/payments?access_token=TEST-7094732018586761-042619-0460da5877116609ef45498be16d8e41-221281264")
+        .send(req.body)
+        .set('Content-Type', 'application/json')
+        .then(function (payStatus) {
+                console.log("Se RealiZo el pago")
+                res.send(payStatus);
+        }).catch(function(err){
+            next(err.response.body.message);
         })
-        .catch(function (err) {
-            //console.log(err);
-            next(err);
-        });
 }
